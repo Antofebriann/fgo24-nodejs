87a7c434e23a64424a66d56e73be422e0e2e68cd
Antofebriann <113361419+Antofebriann@users.noreply.github.com>
2025-04-22 14:44:24 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,31 @@
+//10 list music didalam array
+// buatkan file mp3 didalam folder music
+const fs = require('fs').promises
+const path = require('path')
+async function createFile(fileName){
+    try{
+        const content = "hello"
+        await fs.writeFile(fileName, content)
+        console.log("write file succes")
+    }catch(err){
+        console.log(err)
+    }
+}
+const musicList = [
+    'TWICE - What Is Love',
+    'Linkin Park - Numb',
+    'TWICE - Signal',
+    'TWICE - Fancy',
+    'Linkin Park - Numb',
+    'linkin Park - What ive done',
+    'linkin Park - In The End',
+    'illit - Midnight Fiction',
+    'illit - Magnetic',
+    'illit - Cherrish',
+    'Yung Kai - Blue'
+    
+]
+
+musicList.forEach(music => {
+    createFile(path.join("music", `${music}.mp3`))
+})
